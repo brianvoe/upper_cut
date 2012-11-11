372cc2e38390201ce246ab19281f2ebbc414400a
Brian Voelker <brian@webiswhatido.com>
2012-11-10 21:26:47 -0600

    Add more errors checks and alter type check.

--- uppercut.php
+++ uppercut.php
@@ -1,7 +1,7 @@
 <?php
    	// Options
-	$allowed_types = array('.jpg','.gif','.bmp','.png'); // Allowed file types
-	$max_size = /*10 * */(1024 * 1024); // Maximum filesize in bytes (currently 10MB).
+	$allowed_types = array('jpg','jpeg','gif','bmp','png'); // Allowed file types
+	$max_size = 10 * (1024 * 1024); // Maximum filesize in bytes (currently 10MB).
 	$upload_path = 'uploads/'; // Upload location - trailing slash
 
 	// Return info
@@ -16,12 +16,13 @@
 	$file_width = 0;
 	$file_type = $_FILES[$name]['type'];
 	$file_path = $upload_path.$file_name;
-	$file_ext = substr($file_name, strpos($file_name,'.'), strlen($file_name)-1);
+	// $file_ext = substr($file_name, strpos($file_name,'.'), strlen($file_name)-1); // old way
+	$file_ext = explode('/', $file_type)[1];// substr($file_type, strpos($file_type,'/'), strlen($file_type)-1);
 
 	// Validate
 	if(!in_array($file_ext, $allowed_types)) { // Check file types
 		$return_info['status'] = 'error';
-		$return_info['info'] = 'File type is not allowed';
+		$return_info['info'] = 'File type is not allowed '.$file_ext;
 	}
 	if(filesize($file_temp) > $max_size) { // Check filesize
 		$return_info['status'] = 'error';
@@ -34,22 +35,26 @@
 
 	// Upload file
 	if($return_info['status'] != 'error') {
-		move_uploaded_file($file_temp, $file_path);
+		$move_file = move_uploaded_file($file_temp, $file_path);
+		if(!$move_file) {
+			$return_info['status'] = 'error';
+			$return_info['info'] = 'There was a problem moving image.';
+		} else {
+			// Get file height and width
+			list($file_width, $file_height, $type, $attr) = getimagesize($file_path);
 
-		// Get file height and width
-		list($file_width, $file_height, $type, $attr) = getimagesize($file_path);
-
-		$return_info['status'] = 'success';
-		$return_info['file'] = array(
-			'name' => $file_name,
-			'path' => $file_path,
-			'size' => $file_size,
-			'height' => $file_height,
-			'width' => $file_width,
-			'type' => $file_type,
-			'ext' => $file_ext
-		);
-		$return_info['info'] = '';
+			$return_info['status'] = 'success';
+			$return_info['file'] = array(
+				'name' => $file_name,
+				'path' => $file_path,
+				'size' => $file_size,
+				'height' => $file_height,
+				'width' => $file_width,
+				'type' => $file_type,
+				'ext' => $file_ext
+			);
+			$return_info['info'] = '';
+		}
 	}
 
 	// Return file info
